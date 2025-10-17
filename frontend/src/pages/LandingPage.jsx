6585acda771b5dfd488be17d7551d32e2b18138d
Muco Abu <mbekere1@outlook.com>
2025-10-17 01:22:43 -0700

    added three more images to landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,8 +5,11 @@ import styles from "./LandingPage.module.css";
 import img1 from "../assets/images/property1.jpg";
 import img2 from "../assets/images/property2.jpg";
 import img3 from "../assets/images/property3.jpg";
+import img4 from "../assets/images/property4.jpg";
+import img5 from "../assets/images/property5.jpg";
+import img6 from "../assets/images/property6.jpg";
 
-const images = [img1, img2, img3];
+const images = [img1, img2, img3, img4, img5, img6];
 
 export default function LandingPage() {
   const [currentIndex, setCurrentIndex] = useState(0);
