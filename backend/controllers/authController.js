f4b2c070045baffea97df4bce0404afe3708b3a0
Muco Abu <mbekere1@outlook.com>
2025-10-17 23:03:11 -0700

    added json token for user signing in

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -39,7 +39,7 @@ export const login = async (req, res) => {
     const token = jwt.sign({ userId: user.id, role: user.role }, JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.json({ token });
+    res.json({ token, role: user.role });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
